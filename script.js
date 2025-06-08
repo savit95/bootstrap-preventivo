517f33fd2842d0cc48aeba6f5c2dda56c1fd2d60
savit95 <salvatorevitiello95@gmail.com>
2025-06-08 16:52:55 +0200

    Declared array with promo codes and hours, added values to variables

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const form = document.getElementById("form");
 const selectWork = document.getElementById("select-work");
 const promoCode = document.getElementById("promo-code");
+const promoCodes = ['YHDNU32','JANJC63','PWKCN25','SJDPO96','POCIE24'];
+const hour = 10;
 console.log(form);
 console.log(selectWork);
 console.log(promoCode);
+console.log(promoCodes);
+console.log(hour);
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    let work = selectWork.value;
+    let promo = promoCode.value;
+    console.log(work);
+    console.log(promo);
+
+
+
 });
