5676acd342557f28a4fe0aaa91985127675cc816
savit95 <salvatorevitiello95@gmail.com>
2025-06-17 20:13:48 +0200

    Added function to calculate total work cost based on work type and hours

--- script.js
+++ script.js
@@ -10,13 +10,33 @@ const promoCollection = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 console.log(hour);
 console.log(promoCollection);
 
-form.addEventListener("submit", function(event){
-event.preventDefault();
 
-const work = selectWork.value;
-const promo = promoCode.value;
-console.log(work);
-console.log(promo);
+// Calculates the total cost based on the selected work type and number of hours.
+function calculateWorkCost(work, hour) {
+    if (work === '1') {
+        return hour * 20.50;
+    } else if (work === '2') {
+        return hour * 15.30;
+    } else {
+        return hour * 33.60;
+    }
+}
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const work = selectWork.value;
+    const promo = promoCode.value;
+    console.log(work);
+    console.log(promo);
+
+    // Calls the function to calculate the total cost work
+    let resultCalculateWorkCost = calculateWorkCost(work, hour);
+    console.log(resultCalculateWorkCost);
+
+    // promo code
+
+    //printed price
 
 
 });
