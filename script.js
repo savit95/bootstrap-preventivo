4eaa035189f2568d7795dca1db24f15e5ee6b017
savit95 <salvatorevitiello95@gmail.com>
2025-06-17 19:05:18 +0200

    Added references to form elements and variables for hour and promo code collection

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const selectWork = document.getElementById("select-work");
+const promoCode = document.getElementById("promo-code");
+console.log(form);
+console.log(selectWork);
+console.log(promoCode);
+
+const hour = 10;
+const promoCollection = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+console.log(hour);
+console.log(promoCollection);
+
+
