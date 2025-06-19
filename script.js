665c9a8f1702087104f5894d2e313e6748f5b27c
savit95 <salvatorevitiello95@gmail.com>
2025-06-19 21:08:53 +0200

    Created footer section

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const form = document.getElementById("form");
 const selectWork = document.getElementById("select-work");
 const promoCode = document.getElementById("promo-code");
-console.log(form);
-console.log(selectWork);
-console.log(promoCode);
-
 const hour = 10;
 const promoCollection = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
-console.log(hour);
-console.log(promoCollection);
-
 
 // Calculates the total cost based on the selected work type and number of hours.
 function calculateWorkCost(work, hour) {
@@ -22,7 +15,6 @@ function calculateWorkCost(work, hour) {
     }
 }
 
-
 // Formatting and displaying work cost with Euro currency and decimal separation
 function finalPrice(resultCalculateWorkCost) {
     let fixedRate = resultCalculateWorkCost.toFixed(2);
@@ -31,18 +23,14 @@ function finalPrice(resultCalculateWorkCost) {
     document.getElementById("decimal-part").innerHTML = `,${result[1]} `;
 }
 
-
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const work = selectWork.value;
     const promo = promoCode.value;
-    console.log(work);
-    console.log(promo);
 
     // Calls the function to calculate the total cost work
     let resultCalculateWorkCost = calculateWorkCost(work, hour);
-    console.log(resultCalculateWorkCost);
 
     // Check if the entered promo code is valid
     // If valid, apply a 25% discount to the total cost and display a green success message
@@ -59,7 +47,5 @@ form.addEventListener("submit", function (event) {
     //Calls the function to Formatting and displaying work cost
     finalPrice(resultCalculateWorkCost);
    
-
-
 });
 
